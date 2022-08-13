560f4db9c97858a3e0728822be80b7eaab93123d
amri <achmad.amri@gmail.com>
2022-08-13 13:21:05 +0700

    update

--- src/com/gwt/hris/servlet/LoadOnStartupServlet.java
+++ src/com/gwt/hris/servlet/LoadOnStartupServlet.java
@@ -3,12 +3,12 @@ package com.gwt.hris.servlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
+import org.apache.commons.dbcp.BasicDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.gwt.hris.cron.CronMain;
 import com.gwt.hris.db.Manager;
-import com.gwt.hris.db.exception.DAOException;
 
 public class LoadOnStartupServlet extends HttpServlet {
 	private Logger log = LoggerFactory.getLogger(this.getClass());
@@ -16,11 +16,23 @@ public class LoadOnStartupServlet extends HttpServlet {
 
 	@Override
 	public void init() throws ServletException {
-		try {
-			Manager.getInstance().defaultConfigure();
-		} catch (DAOException e) {
-			e.printStackTrace();
-		}
+		BasicDataSource dataSource = new BasicDataSource();
+
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUsername("root");
+		dataSource.setPassword("P@55w0rd");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/hris?autoReconnect=true");
+		dataSource.setMaxActive(1000);
+		dataSource.setMaxIdle(5);
+		dataSource.setInitialSize(5);
+		dataSource.setValidationQuery("SELECT 1");
+		
+		Manager.getInstance().setDataSource(dataSource);
+//		try {
+//			Manager.getInstance().defaultConfigure();
+//		} catch (DAOException e) {
+//			e.printStackTrace();
+//		}
 		
 		CronMain cronMain = new CronMain();
 		cronMain.run();
